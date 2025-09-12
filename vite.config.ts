9857a7d1b2558f30b889b1df654b9b1f3f8aceb2
GMCandres <andriuuuuu56@live.com>
2025-09-12 14:32:29 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/GMC/',
+  base: '/GMC/', // <-- debe coincidir con el nombre de tu repo (case-sensitive)
+})
